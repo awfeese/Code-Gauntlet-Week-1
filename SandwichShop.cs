bf850729d18211df45c51a58bcc964a92d77e0c0
Andrew Feese <feese83@gmail.com>
2015-07-22 09:16:05 -0500

    changed format of output

--- SandwichShop.cs
+++ SandwichShop.cs
@@ -1,13 +1,19 @@
 // C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /out:SandwichShop.exe *.cs
 
 public class SandwichShop {
-  public static void Main(string[] args) {    
-    Sandwich sandwich1 = new Large(new Cheese(new Bacon(new TurkeySandwich())));
-    System.Console.WriteLine("Large Cheese, Bacon, and Turkey Sandwich: ${0}", string.Format("{0:0.00}", sandwich1.Price));
+  
+  public static void Main(string[] args) {
     
-    System.Console.WriteLine();
+    System.Console.WriteLine("\nSandwich Shop\n");
+    
+    Sandwich sandwich1 = new Large(new Cheese(new Bacon(new TurkeySandwich())));
+    System.Console.WriteLine("Large Cheese, Bacon, and Turkey Sandwich: ${0}",
+      string.Format("{0:0.00}", sandwich1.Price));
     
     Sandwich sandwich2 = new Regular(new Avocado(new SauteedOnion(new Cheese(new RoastBeefSandwich()))));
-    System.Console.WriteLine("Regular Avocado, Sautéed Onions, Cheese, and Roast Beef Sandwich: ${0}", string.Format("{0:0.00}", sandwich2.Price));
+    System.Console.WriteLine("Regular Avocado, Sautéed Onions, Cheese, and Roast Beef Sandwich: ${0}",
+      string.Format("{0:0.00}", sandwich2.Price));
+      
+    System.Console.WriteLine();
   }
 }
